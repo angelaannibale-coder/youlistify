7f05c78062cc4e876e1c39b3c2b2241b7dd34e59
angelaannibale-coder <angelaannibale@gmail.com>
2026-08-15 05:55:07 -0400

    Update page.tsx

--- app/list-service/page.tsx
+++ app/list-service/page.tsx
@@ -50,6 +50,7 @@ useEffect(() => {
 }, []);
 
   async function createAccount() {
+    alert(Create account button is working");
 if (!form.email || !password) {
 alert("Please enter a password.");
 return;
