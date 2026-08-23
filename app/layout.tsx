f088bef55096b1fdcdc12e6ff7192141e29123c6
angelaannibale-coder <angelaannibale@gmail.com>
2026-08-23 15:45:36 -0400

    Load homepage profile card tilt styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./categories-polish.css";
+import "./home-card-tilt.css";
 import SafetyFooterLink from "./SafetyFooterLink";
 import LaunchPolish from "./LaunchPolish";
 import FoundingProviderOffer from "./FoundingProviderOffer";
